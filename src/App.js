9ac07e54e4aa6b62ff0bd2f7a60ae1dd32fa4b39
SharPinG <spg1101@naver.com>
2022-08-29 19:02:01 +0900

    m

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100vh',
+    }}>
+      <p style={{
+        fontSize: 30,
+        fontWeight: 'bold',
+      }}>
+        Hello World!
+      </p>
+    </div>
+  );
+}
